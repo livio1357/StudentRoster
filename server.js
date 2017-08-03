0ca5a2f3edcd5b665f37046e43888345b615a3e9
livio bolzon <livio@actingstudiochicago.com>
2017-08-02 20:43:45 -0500

    adding fun stuff

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+//NPM express
+
 var express = require("express");
 var app = express();
-
+// Port that the computer is listening on.
 var PORT = 3000; 
+
+app.get("/", function(req, res) {
+
+  res.send("<marquee> WLLLLLLLLLLLLLCOMMOEMOMOEMEOEMOMEOEMOEEMEOM! </marquee>");
+
+});
 
 app.listen(PORT, function(){
 
 
     console.log(`Listening on PORT ${PORT}`);
 });
+
